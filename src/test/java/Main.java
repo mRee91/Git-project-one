0f2b19f3580bfd791660b3d31042d2e188e0c8e5
mRee91 <no.1@gmx.com>
2022-12-17 19:18:14 +0300

    version 3.2

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,12 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("The first version is complete");
         // add .   stage
-        //git commit -m "message"    commit
+        // add {foldername} only stage that folder
+        //git commit -m "message"    commit to local repo // each commit will not be overwritten
+
+
         //wpd shows current path
         //ls shows you what5 folders are available
-
-        
-        // cd and folder name takes you to that folder
-        //
+        // cd {folder name} takes you to that folder
+        // cd ..  move back a fokder
     }
 }
