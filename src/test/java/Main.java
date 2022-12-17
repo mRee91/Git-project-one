6081d75fce09ac0262dd1de1a6461053ba36154d
mRee91 <no.1@gmx.com>
2022-12-17 19:08:55 +0300

    version3

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("The first version is complete");
+        // add .   stage
+        //git commit -m "message"    commit
+        //wpd shows current path
+        //ls shows you what5 folders are available
 
+        
+        // cd and folder name takes you to that folder
+        //
     }
 }
